c28bb8f2a0b94dbcdb5f7594b5df80da957b9408
Abylay <a.madalkhan@gmail.com>
2021-05-11 01:17:01 +0600

    Add SecurityFilter for check access to servlets use session.

---
+++ src/main/java/com/planets/filters/SecurityFilter.java
@@ -0,0 +1,41 @@
+package com.planets.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+import com.planets.model.User;
+
+@WebFilter(filterName = "SecurityFilter", urlPatterns = "/admin/*")
+public class SecurityFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        String URI = httpServletRequest.getRequestURI();
+
+        HttpSession session = httpServletRequest.getSession();
+        User user = (User) session.getAttribute("current-user");
+
+        if (URI.equals("/admin") && user == null) {
+            httpServletResponse.sendRedirect("/admin/login");
+            return;
+        }
+
+        if (URI.equals("/admin/login") && user != null) {
+            httpServletResponse.sendRedirect("/admin");
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+}
