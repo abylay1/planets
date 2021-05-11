653e21320373e3f0f8b31b7076cade594e2ccd4b
Abylay <a.madalkhan@gmail.com>
2021-05-12 02:12:17 +0600

    Add getBigPlanets method for BigPlanetServlet and getSmallPlanets method for SmallPlanetServlet.

--- src/main/java/com/planets/utils/PlanetDAO.java
+++ src/main/java/com/planets/utils/PlanetDAO.java
@@ -50,4 +50,77 @@ public class PlanetDAO {
         return planets;
     }
 
+    public ArrayList<Planet> getBigPlanets() {
+        ArrayList<Planet> planets = new ArrayList<Planet>();
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+
+            try (Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)){
+
+                Statement statement = conn.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM planets WHERE big = 1");
+
+                while(resultSet.next()){
+
+                    int id = resultSet.getInt(1);
+                    String name = resultSet.getString(2);
+                    String mass = resultSet.getString(3);
+                    int area = resultSet.getInt(4);
+                    int distance = resultSet.getInt(5);
+                    int periodCircle = resultSet.getInt(6);
+                    double radius = resultSet.getDouble(7);
+                    double gravity = resultSet.getDouble(8);
+                    boolean small = resultSet.getBoolean(9);
+                    boolean big = resultSet.getBoolean(10);
+                    String image = resultSet.getString(11);
+
+                    Planet planet = new Planet(id, name, mass, area, distance, periodCircle, radius, gravity, big, small, image);
+                    planets.add(planet);
+                }
+            }
+        }
+        catch (Exception exception){
+            System.err.println("Some error with DataBase in PlanetDAO.java");
+            System.err.println(exception);
+        }
+        return planets;
+    }
+
+    public ArrayList<Planet> getSmallPlanets() {
+        ArrayList<Planet> planets = new ArrayList<Planet>();
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+
+            try (Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)){
+
+                Statement statement = conn.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM planets WHERE small = 1");
+
+                while(resultSet.next()){
+
+                    int id = resultSet.getInt(1);
+                    String name = resultSet.getString(2);
+                    String mass = resultSet.getString(3);
+                    int area = resultSet.getInt(4);
+                    int distance = resultSet.getInt(5);
+                    int periodCircle = resultSet.getInt(6);
+                    double radius = resultSet.getDouble(7);
+                    double gravity = resultSet.getDouble(8);
+                    boolean small = resultSet.getBoolean(9);
+                    boolean big = resultSet.getBoolean(10);
+                    String image = resultSet.getString(11);
+
+                    Planet planet = new Planet(id, name, mass, area, distance, periodCircle, radius, gravity, big, small, image);
+                    planets.add(planet);
+                }
+            }
+        }
+        catch (Exception exception){
+            System.err.println("Some error with DataBase in PlanetDAO.java");
+            System.err.println(exception);
+        }
+        return planets;
+    }
 }
