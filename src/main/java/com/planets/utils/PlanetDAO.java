b5c0fcd318c687ca922d5faecd631713738e93d8
Abylay <a.madalkhan@gmail.com>
2021-05-13 13:44:05 +0600

    Add get planet by id and fix exception.

--- src/main/java/com/planets/utils/PlanetDAO.java
+++ src/main/java/com/planets/utils/PlanetDAO.java
@@ -13,6 +13,45 @@ public class PlanetDAO {
 
     }
 
+    public Planet getPlanetByID(int ID) {
+        Planet planet = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+
+            try (Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)){
+
+                String sql = "SELECT * FROM planets WHERE idplanet = ?";
+
+                try (PreparedStatement preparedStatement = conn.prepareStatement(sql)){
+                    preparedStatement.setInt(1, ID);
+                    ResultSet resultSet = preparedStatement.executeQuery();
+
+                    if(resultSet.next()){
+                        int id = resultSet.getInt(1);
+                        String name = resultSet.getString(2);
+                        String mass = resultSet.getString(3);
+                        int area = resultSet.getInt(4);
+                        int distance = resultSet.getInt(5);
+                        int periodCircle = resultSet.getInt(6);
+                        double radius = resultSet.getDouble(7);
+                        double gravity = resultSet.getDouble(8);
+                        boolean small = resultSet.getBoolean(9);
+                        boolean big = resultSet.getBoolean(10);
+                        String image = resultSet.getString(11);
+
+                        planet = new Planet(id, name, mass, area, distance, periodCircle, radius, gravity, big, small, image);
+                    }
+                }
+            }
+        }
+        catch (Exception exception){
+            System.err.println("Some error with DataBase in PlanetDAO.java");
+            exception.printStackTrace();
+        }
+        return planet;
+    }
+
     public ArrayList<Planet> getAllPlanets() {
         ArrayList<Planet> planets = new ArrayList<Planet>();
 
@@ -45,7 +84,7 @@ public class PlanetDAO {
         }
         catch (Exception exception){
             System.err.println("Some error with DataBase in PlanetDAO.java");
-            System.err.println(exception);
+            exception.printStackTrace();
         }
         return planets;
     }
@@ -119,7 +158,7 @@ public class PlanetDAO {
         }
         catch (Exception exception){
             System.err.println("Some error with DataBase in PlanetDAO.java");
-            System.err.println(exception);
+            exception.printStackTrace();
         }
         return planets;
     }
